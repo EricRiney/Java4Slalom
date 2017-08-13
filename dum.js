2802115bce51def06ca92b37cbb10578445b0aec
EricRiney <fails.to.install@gmail.com>
2017-08-13 10:43:41 -0700

    back to go

--- dum.js
+++ dum.js
@@ -53,15 +53,54 @@
 //   // All things are done!
 // });
 
-var request = require('request');
-var meow = request('http://127.0.0.1:9090', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+// var request = require('request');
+// var meow = request('http://127.0.0.1:9090', function (error, response, body) {
+//   console.log('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+// });
 
-for (var i = 0; i < 10; i++) {
-    meow;
-    console.log(i);
-    meow;
-}
+// for (var i = 0; i < 10; i++) {
+//     meow;
+//     console.log(i);
+//     meow;
+// }
+
+
+var http = require('http');
+var async = require('async');
+
+var options = {
+  hostname: '127.0.0.1',
+  port: 9090,
+  path: '/',
+  method: 'GET'
+};
+
+function sendRequestWrapper(n, done){
+  console.log('Calling sendRequest', n);
+  sendRequest(options, function(err){
+    done(err);
+  });
+};
+
+function sendRequest(options, callback){
+  //console.log('hello');
+  var start = new Date();
+  var req = http.request(options,function(res) {
+    // I don't know if this callback is called for error responses
+    // I have only used the `request` library which slightly simplifies this
+    // Under some circumstances you can accidentally cause problems by calling
+    // your callback function more than once (e.g. both here and on('error')
+
+    console.log('Request took:', new Date() - start, 'ms');
+    callback(null);
+  });
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+    callback(e);
+  });
+  req.end();
+};
+
+async.timesSeries(10, sendRequestWrapper);
