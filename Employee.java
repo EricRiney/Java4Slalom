f3ab05a6bfba9c749df429a7e44223b7a6914456
EricRiney <fails.to.install@gmail.com>
2017-08-16 10:57:52 -0700

    push

--- Employee.java
+++ Employee.java
@@ -1,31 +1,36 @@
 import java.io.*;
 public class Employee {
 
-   // this instance variable is visible for any child class.
-   public String name;
+   String name;
+   int age;
+   String designation;
+   double salary;
 
-   // salary  variable is visible in Employee class only.
-   private double salary;
-
-   // The name variable is assigned in the constructor.
-   public Employee (String empName) {
-      name = empName;
+   // This is the constructor of the class Employee
+   public Employee(String name) {
+      this.name = name;
    }
 
-   // The salary variable is assigned a value.
-   public void setSalary(double empSal) {
-      salary = empSal;
+   // Assign the age of the Employee  to the variable age.
+   public void empAge(int empAge) {
+      age = empAge;
    }
 
-   // This method prints the employee details.
-   public void printEmp() {
-      System.out.println("name  : " + name );
-      System.out.println("salary :" + salary);
+   /* Assign the designation to the variable designation.*/
+   public void empDesignation(String empDesig) {
+      designation = empDesig;
    }
 
-   public static void main(String args[]) {
-      Employee empOne = new Employee("Ransika");
-      empOne.setSalary(1000);
-      empOne.printEmp();
+   /* Assign the salary to the variable	salary.*/
+   public void empSalary(double empSalary) {
+      salary = empSalary;
+   }
+
+   /* Print the Employee details */
+   public void printEmployee() {
+      System.out.println("Name:"+ name );
+      System.out.println("Age:" + age );
+      System.out.println("Designation:" + designation );
+      System.out.println("Salary:" + salary);
    }
 }
